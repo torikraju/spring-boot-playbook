201ccaa1f24dec717024ff70e182ea167176f745
torikul <torikul.alam@reddotdigitalit.com>
2021-11-28 18:56:00 +0600

    adjust remote logs

--- src/main/java/com/self/learning/playbook/resource/CustomerResource.java
+++ src/main/java/com/self/learning/playbook/resource/CustomerResource.java
@@ -2,8 +2,8 @@ package com.self.learning.playbook.resource;
 
 import com.self.learning.playbook.annotation.ApiController;
 import com.self.learning.playbook.assembler.CustomerModelAssembler;
-import com.self.learning.playbook.dto.CustomerDto;
 import com.self.learning.playbook.dto.CustomerAllResponse;
+import com.self.learning.playbook.dto.CustomerDto;
 import com.self.learning.playbook.dto.CustomerResponse;
 import com.self.learning.playbook.entity.Customer;
 import com.self.learning.playbook.repository.CustomerRepository;
@@ -11,14 +11,13 @@ import com.self.learning.playbook.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
